2a1e83b387f3ac29bfbf4c8cac39b9aa4cf27e0b
TaRRoS-Dev <kyllianevrard1623@gmail.com>
2025-09-22 16:37:31 +0200

    initial commit

---
+++ src/main/java/com/spingluck/SpringLuck/SpringLuckApplication.java
@@ -0,0 +1,13 @@
+package com.spingluck.SpringLuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLuckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spingluck/SpringLuck/SpringLuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spingluck.SpringLuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
