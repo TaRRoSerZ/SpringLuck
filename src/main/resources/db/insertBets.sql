b3b20e02309407ee28eb9845455dd62aeb340e41
TaRRoS-Dev <kyllianevrard1623@gmail.com>
2025-10-20 14:51:58 +0200

    feat : add adapter.out.persistence classes and tests

---
+++ src/main/resources/db/insertBets.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.bets (amount, date, isWinningBet) VALUES (100.00, '2024-06-01 10:00:00', true);
+INSERT INTO public.bets (amount, date, isWinningBet) VALUES (50.00, '2024-06-02 11:30:00', false);
