f0ede1050b15577aecf7adc27af0b6e6fa5318fc
Kakayano <neo.herbigniaux@std.heh.be>
2026-01-17 21:23:04 +0100

    final version

--- src/main/java/com/spingluck/SpringLuck/application/domain/service/UserService.java
+++ src/main/java/com/spingluck/SpringLuck/application/domain/service/UserService.java
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.yaml.snakeyaml.DumperOptions.LineBreak.WIN;
 
 @RequiredArgsConstructor
 public class UserService implements UserUseCase {
